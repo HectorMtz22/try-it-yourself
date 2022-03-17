9cb58fe97215ab426122b5d73838aaf4dd1aadcc
HectorMtz22 <hec.gre81@gmail.com>
2022-03-16 22:39:39 -0600

    Add a style for doing SafeAreaView on Android

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import { SafeAreaView, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
+  const insets = useSafeAreaInsets();
   return (
-    <SafeAreaView>
+    <View style={styles(insets).container}>
       <Text>Home Screen</Text>
-    </SafeAreaView>
+    </View>
   );
 }
+
+const styles = (insets) =>
+  StyleSheet.create({
+    container: {
+      paddingTop: insets.top,
+      paddingBottom: insets.bottom,
+      paddingHorizontal: 16,
+      flex: 1,
+    },
+  });
